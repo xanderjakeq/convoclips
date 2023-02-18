11c240b4822b2394d4c2a063754156fc64912f0c
xanderjakeq <xanderjakeq@pop-os.localdomain>
2023-02-18 17:52:51 -0500

    hotfix

--- sveltekit/src/routes/explore/+page.svelte
+++ sveltekit/src/routes/explore/+page.svelte
@@ -7,8 +7,8 @@
 	let clips: ClipType[] = data.clips;
 </script>
 
-<div class="md:container md:mx-auto flex items-center justify-center min-h-[80vh]">
-	<div class="w-full flex flex-col font-mono max-w-3xl break-words">
+<div class="md:container md:mx-auto flex justify-center min-h-[80vh]">
+	<div class="w-full flex flex-col font-mono w-full max-w-3xl break-words">
 		<h1 class="my-5 text-2xl font-bold">recent clips</h1>
 		{#each clips as clip (clip.id)}
 			<Clip {clip} isPreview={true} />
--- sveltekit/src/routes/server/[id]/+page.svelte
+++ sveltekit/src/routes/server/[id]/+page.svelte
@@ -8,8 +8,8 @@
 	const { server, clips } = data;
 </script>
 
-<div class="md:container md:mx-auto flex items-center justify-center min-h-[80vh]">
-	<div class="flex flex-col font-mono max-w-3xl break-words">
+<div class="md:container md:mx-auto flex justify-center min-h-[80vh]">
+	<div class="flex flex-col font-mono w-full max-w-3xl break-words">
 		<h1 class="my-5 text-2xl font-bold">{server.name}</h1>
 		{#if server.website}
 			<a href={server.website} class="my-5 text-sm font-italic">{server.website}</a>
