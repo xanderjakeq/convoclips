703dab9db06a3c40e02469d6f5d8860b2c58dae0
xanderjakeq <xanderjakeq@pop-os.localdomain>
2023-02-18 17:52:20 -0500

    sync schema with bot

--- sveltekit/src/lib/z.ts
+++ sveltekit/src/lib/z.ts
@@ -1,12 +1,13 @@
 import { z } from "zod";
 
 //I need z schemas in one place shared between bot and sveltekit
+//maybe some script to copy this file over to bot.
 export const websiteSchema = z.string().url();
 
 export const serverSchema = z.object({
 	dc_guildId: z.string(),
 	name: z.string(),
-	website: websiteSchema.optional(),
+	website: websiteSchema.nullish(),
 	ownerId: z.string(),
 });
 
